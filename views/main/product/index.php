97904b3c1e9bb4ed39c4c4d35ddf1849912ae1bd
nguyuyen <nguyen.nguyennguyuyen@hcmut.edu.vn>
2023-12-07 13:24:14 +0700

    modify product display (to vietnamese)

--- views/main/product/index.php
+++ views/main/product/index.php
@@ -17,19 +17,19 @@
                 <div class="row">
                     <form action="index.php?page=main&controller=cart&action=addHidden" method="post">
                     <div class="input-group input-group-lg rounded-pill">
-                        <span class="input-group-text fw-semibold" id="inputGroup-sizing-lg">Select Quantity</span>
+                        <span class="input-group-text fw-semibold" id="inputGroup-sizing-lg">Số lượng</span>
                         <input type="hidden" name="product_id" value="<?php echo $product->id;?>">
                         <input name="num" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"  value="1">
                     </div>
-                        <button type="submit" class="d-block w-100 btn btn-dark rounded-pill fw-semibold py-3 my-3" id="addCart">Add to cart</button>
+                        <button type="submit" class="d-block w-100 btn btn-dark rounded-pill fw-semibold py-3 my-3" id="addCart">Thêm vào giỏ hàng</button>
                     </form>
                 </div>
                 
 
                 <p class="decription lh-lg fw-semibold"><?php echo $product->product_note;?></p>
                 <ul>
-                    <li>Colour Shown: <?php echo $product->color; ?></li>
-                    <li>Style: <?php echo $product->style; ?></li>
+                    <li>Màu sắc: <?php echo $product->color; ?></li>
+                    <li>Kiểu: <?php echo $product->style; ?></li>
                 </ul>
             </div>
         </div>
@@ -39,7 +39,7 @@
             <div class="d-none d-sm-block border-bottom border-2 mb-4"></div>
                 <p class="d-inline-flex gap-1">
                     <a  class="link-secondary link-underline-opacity-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        <h5 class ="d-inline">All Reviews</h5>
+                        <h5 class ="d-inline">Đánh giá</h5>
  
                     </a>
                     <span class="badge text-bg-secondary"><?php echo sizeof($reviews)?></span>
@@ -60,8 +60,8 @@
                 <form action="index.php?page=main&controller=product&action=insertReview" method="post">
                     <input type="hidden" name="product_id" value="<?php echo $product->id?>">
                     <div class="input-group mb-3">
-                        <input type="text" name="content" class="form-control" placeholder="Write your review about this product" aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn btn-dark" type="submit" id="button-addon2">Write review</button>
+                        <input type="text" name="content" class="form-control" placeholder="Viết đánh giá của bạn về sản phẩm này" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-dark" type="submit" id="button-addon2">Viết đánh giá</button>
                     </div>
                 </form>
             </div>
